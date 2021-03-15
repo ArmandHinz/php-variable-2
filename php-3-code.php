fe0b2016aa4aedddf4782e416d741f20def5e547
ArmandHinz <Armand.hinz@gmail.com>
2021-03-15 10:20:51 +0100

    exercice code

---
+++ php-3-code.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>
+        <?php
+
+        $message1 = "0@sn9sirppa@#?ia'jgtvryko1";
+        //j'ai appris
+        $message2 = "q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj";
+        //toutes les ficelles
+        $message3 ="aopi?sgnirts@#?sedhtg+p9l!";
+        //des strings
+
+    //Première étape
+        $longueur = strlen($message3) / 2;
+    // dexième étape
+        $sousChaine = substr($message3, 5, $longueur);
+    // trosième étape
+        $remplace = str_replace('@#?', ' ', $sousChaine);
+    // quatrième étape
+        echo strrev($remplace);
+        ?>
+</p>
+
+
+</body>
+</html>
