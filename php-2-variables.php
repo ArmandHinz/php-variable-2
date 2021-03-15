946cd6ca4467ee25946e6b345fd6c2a66cd3223b
ArmandHinz <Armand.hinz@gmail.com>
2021-03-15 09:51:18 +0100

    execice

---
+++ php-2-variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php 
+        $nomFilmVo = 'Indiana Jones and the Last Crusade';
+        echo $nomFilmVo;
+
+        $dejaVu = false;
+        echo $dejaVu;
+
+        $SortieSalle = 1989;
+        echo $SortieSalle;
+
+        $noteFilm = 8.2;
+        echo $nomFilmVo;
+        ?>
+</body>
+</html>
+
