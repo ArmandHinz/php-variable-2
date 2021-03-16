3e4ad7c79301a3add314509e861db58de53bb536
ArmandHinz <Armand.hinz@gmail.com>
2021-03-16 11:31:30 +0100

    exercice débug 7

---
+++ debug-7.php
@@ -0,0 +1,24 @@
+<?php
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Armand" => [
+        "city" => "Selestat",
+        "weapon" => "Lebel 1866"
+    ]
+];
+
+var_dump($characters);
+
+// liens de la capture 
+https://imgur.com/86AxWGV
